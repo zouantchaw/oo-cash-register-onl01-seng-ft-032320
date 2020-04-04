c51eb61ea7b828525a9d7f87e247e8c2dc4ee34a
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-04 22:38:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,2 +1,17 @@
 class CashRegister 
+  attr_accessor :items, :total, :discount, :last_transaction 
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+ 
+  def add_item(item, price, quantity=1)
+    quantity.times {
+      @items << item
+    }
+    self.total += price * quantity
+    @last_transaction = price * quantity
+  end
 end 
